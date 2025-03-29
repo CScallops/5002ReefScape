ac68c1725d986f1fb4ec870a082ba094e34a8813
CScallops <145216836+CScallops@users.noreply.github.com>
2025-03-29 08:36:13 -0500

    Derivative on PV.

--- src/main/java/frc/robot/DR_PID.java
+++ src/main/java/frc/robot/DR_PID.java
@@ -1,4 +1,4 @@
-package frc.robot.controller;
+package frc.robot;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.MathUtil;
@@ -8,52 +8,60 @@ import edu.wpi.first.math.MathUtil;
 // CVn = CVn-1 + Kp*(En - En-1) + Ki*En*dt + Kd*(En -2*En-1 +En-2)/td
 public class DR_PID extends PIDController {
   // Factor for "proportional" control
-  private double m_kp;
+  protected double m_kp;
   // Factor for "integral" control
-  private double m_ki;
+  protected double m_ki;
   // Factor for "derivative" control
-  private double m_kd;
+  protected double m_kd;
   // The error range where "integral" control applies
-  private double m_iZone = Double.POSITIVE_INFINITY;
+  protected double m_iZone = Double.POSITIVE_INFINITY;
   // The period (in seconds) of the loop that calls the controller
-  private final double m_period;
-  private double m_maximumIntegral = 1.0;
-  private double m_minimumIntegral = -1.0;
-  private double m_maximumInput;
-  private double m_minimumInput;
+  protected final double m_period;
+  protected double m_maximumIntegral = 1.0;
+  protected double m_minimumIntegral = -1.0;
+  protected double m_maximumInput;
+  protected double m_minimumInput;
   // Do the endpoints wrap around? e.g. Absolute encoder
-  private boolean m_continuous;
+  protected boolean m_continuous;
   // The error at the time of the most recent call to calculate()
-  private double m_error;
-  private double m_errorDerivative;
+  protected double m_error;
+  protected double m_errorDerivative;
   // The error at the time of the second-most-recent call to calculate() (used to compute velocity)
-  private double m_prevError = 0.0;
-	private double m_setpoint;
-  private double m_measurement;
-  private boolean m_haveMeasurement;
-  private boolean m_haveSetpoint;
+  protected double m_prevError = 0.0;
+  protected double m_setpoint;
+  protected double m_measurement;
+  protected boolean m_haveMeasurement = false;
+  protected boolean m_haveSetpoint;
 
-	double m_output = 0.0;
-	private double m_prevOutput = 0.0;
-	private double m_prevPrevError = 0.0;
+  double m_output = 0.0;
+  protected double m_prevOutput = 0.0;
+  protected double m_prevMeas = 0.0;
+  protected double m_prevPrevMeas = 0.0;
 
-	public DR_PID(double kp, double ki, double kd) {
-		super(kp, ki, kd, 0.02);
-		this.m_period = 0.02;
-	}
-	public DR_PID(double kp, double ki, double kd, double period) {
-		super(kp, ki, kd, period);
-		this.m_period = period;
-	}
+  public DR_PID(double kp, double ki, double kd) {
+    super(kp, ki, kd, 0.02);
+    this.m_period = 0.02;
+  }
+  public DR_PID(double kp, double ki, double kd, double period) {
+    super(kp, ki, kd, period);
+    this.m_period = period;
+  }
 
-	// CVn = CVn-1 + Kp*(En - En-1) + Ki*En*dt + Kd*(En -2*En-1 +En-2)/td
-	@Override
-	public double calculate(double measurement) {
+  @Override
+  public double calculate(double measurement) {
     m_measurement = measurement;
-		m_prevPrevError = m_prevError;
-    m_prevError = m_error;
+    m_prevOutput = m_output;
+
+    if (m_haveMeasurement) {
+      m_prevPrevMeas = m_prevMeas;
+      m_prevMeas = m_measurement;
+      m_prevError = m_error;
+    } else {
+      // First run, must initialize internal variables.
+      m_prevPrevMeas = m_measurement;
+      m_prevMeas = m_measurement;
+    }
     m_haveMeasurement = true;
-		m_prevOutput = m_output;
 
     if (m_continuous) {
       double errorBound = (m_maximumInput - m_minimumInput) / 2.0;
@@ -62,11 +70,13 @@ public class DR_PID extends PIDController {
       m_error = m_setpoint - m_measurement;
     }
 
+    // CVn = CVn-1 + Kp*(En - En-1) + Ki*En*dt + Kd*(PVn -2*PVn-1 +PVn-2)/td
+  
     // If the absolute value of the position error is greater than IZone, reset the total error
-		double integral;
+    double integral;
     if (Math.abs(m_error) > m_iZone) {
-			integral = 0.0;
-		} else {
+      integral = 0.0;
+    } else {
       integral =
           MathUtil.clamp(
               m_ki * m_error * m_period,
@@ -74,15 +84,17 @@ public class DR_PID extends PIDController {
               m_maximumIntegral);
     }
 
-		double deltaError = (m_error - m_prevError);
+    double deltaError = (m_error - m_prevError);
     m_errorDerivative = deltaError / m_period;
 
-		double derivative = MathUtil.clamp(
-			m_kd * (deltaError + m_prevPrevError - m_prevError)/m_period,
-			m_minimumIntegral, m_maximumIntegral);
+    double secDerivMeas = (measurement -2* m_prevMeas + m_prevPrevMeas)/m_period;
+
+    double derivative = MathUtil.clamp(
+      m_kd * secDerivMeas,
+      m_minimumIntegral, m_maximumIntegral);
 
     m_output = m_prevOutput + m_kp * deltaError + integral + derivative;
-		return m_output;
+    return m_output;
   }
 
 }
